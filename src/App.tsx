ed7498cce3b2bea517f9d5cf636078430a953208
yuu-eguci <yuu.eguci@gmail.com>
2024-10-10 17:51:29 +0900

    ✨ 【ここからビジネスロジック】 4画面のベースを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import '@/App.css'
-import HomePage from '@/pages/HomePage'
+import LoginPage from '@/pages/LoginPage'
+import MainMenuPage from '@/pages/MainMenuPage'
+import TablePage from '@/pages/TablePage'
+import VariousControlsPage from '@/pages/VariousControlsPage'
+
 import NotFoundPage from '@/pages/NotFoundPage'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<MainMenuPage />} />
+      <Route path="/table" element={<TablePage />} />
+      <Route path="/various-controls" element={<VariousControlsPage />} />
       {/* その他のパスは 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
---
+++ src/pages/MainMenuPage.tsx
@@ -0,0 +1,26 @@
+import Button from '@/components/Button'
+import formatDate from '@/utils/formatDate'
+import { useEffect, useState } from 'react'
+
+function MainMenuPage() {
+  const [formattedDate, setFormattedDate] = useState<string>('')
+
+  useEffect(() => {
+    const currentDate = new Date()
+    const dateStr = formatDate(currentDate)
+    setFormattedDate(dateStr)
+  }, [])
+
+  const handleClick = () => {
+    alert('Button clicked!')
+  }
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page - {formattedDate}</h1>
+      <Button label="Click Me" onClick={handleClick} />
+    </div>
+  )
+}
+
+export default MainMenuPage
