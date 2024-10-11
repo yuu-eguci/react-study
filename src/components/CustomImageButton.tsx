0f90ab46a0dbb01f751c163eccdd23968ad9770b
yuu-eguci <yuu.eguci@gmail.com>
2024-10-11 10:36:30 +0900

    ✨ MainMenuPage mui ちっくな画像ボタンを3つ並べる

---
+++ src/components/CustomImageButton.tsx
@@ -0,0 +1,130 @@
+import ButtonBase from '@mui/material/ButtonBase'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+import { useNavigate } from 'react-router-dom'
+
+// ImageType の定義
+export interface ImageType {
+  url: string
+  title: string
+  width: string
+  link: string
+  icon: JSX.Element
+}
+
+// コンポーネントのプロパティの型定義
+interface CustomImageButtonProps {
+  image: ImageType
+}
+
+// styled コンポーネントの定義
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 150,
+  width: '100%',
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important',
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}))
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+})
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}))
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}))
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}))
+
+function CustomImageButton({ image }: CustomImageButtonProps) {
+  const navigate = useNavigate()
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        borderRadius: 2,
+        overflow: 'hidden',
+        mb: 2,
+      }}
+    >
+      <ImageButton
+        focusRipple
+        aria-label={image.title}
+        onClick={() => navigate(image.link)}
+      >
+        <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+        <ImageBackdrop className="MuiImageBackdrop-root" />
+        <Image>
+          <Typography
+            component="span"
+            variant="subtitle1"
+            color="inherit"
+            sx={(theme) => ({
+              display: 'flex',
+              alignItems: 'center',
+              gap: theme.spacing(1),
+              p: 4,
+              pt: 2,
+              pb: `calc(${theme.spacing(1)} + 6px)`,
+            })}
+          >
+            {image.icon}
+            {image.title}
+            <ImageMarked className="MuiImageMarked-root" />
+          </Typography>
+        </Image>
+      </ImageButton>
+    </Paper>
+  )
+}
+
+export default CustomImageButton
--- src/pages/MainMenuPage.tsx
+++ src/pages/MainMenuPage.tsx
@@ -1,24 +1,57 @@
-import Button from '@/components/Button'
-import formatDate from '@/utils/formatDate'
-import { useEffect, useState } from 'react'
+import CustomImageButton from '@/components/CustomImageButton'
+import Box from '@mui/material/Box'
+
+// アイコンのインポート
+import LogoutIcon from '@mui/icons-material/Logout'
+import TableChartIcon from '@mui/icons-material/TableChart'
+import ToysIcon from '@mui/icons-material/Toys'
+
+// 画像のインポート（@/assets/images の中にある画像を使う場合）
+import cityImg from '@/assets/images/bg_city.jpg'
+import seasideImg from '@/assets/images/bg_seaside.jpg'
+import wavingImg from '@/assets/images/bg_waving.jpg'
+
+const images = [
+  {
+    url: cityImg,
+    title: 'メインの入力ページ',
+    width: '100%',
+    link: '/table',
+    icon: <TableChartIcon />,
+  },
+  {
+    url: seasideImg,
+    title: 'いろいろなコントロール',
+    width: '100%',
+    link: '/various-controls',
+    icon: <ToysIcon />,
+  },
+  {
+    url: wavingImg,
+    title: 'ログアウト',
+    width: '100%',
+    link: '/login',
+    icon: <LogoutIcon />,
+  },
+]
 
 function MainMenuPage() {
-  const [formattedDate, setFormattedDate] = useState<string>('')
-
-  useEffect(() => {
-    const currentDate = new Date()
-    const dateStr = formatDate(currentDate)
-    setFormattedDate(dateStr)
-  }, [])
-
-  const handleClick = () => {
-    alert('Button clicked!')
-  }
-
   return (
     <div>
-      <h1>Welcome to the Home Page - {formattedDate}</h1>
-      <Button label="Click Me" onClick={handleClick} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          minWidth: 300,
+          width: '100%',
+          gap: 2,
+        }}
+      >
+        {images.map((image) => (
+          <CustomImageButton key={image.url} image={image} />
+        ))}
+      </Box>
     </div>
   )
 }
